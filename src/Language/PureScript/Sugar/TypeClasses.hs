f3a86eb421bd3b48a1168cc0dd7025fdaf04114d
gnumonik <gnumonik@protonmail.com>
2024-01-23 22:16:44 -0500

    Adjusted typeclass desugaring to use real source locations in the case expressions, necessary for fully typing the desugared typeclass declarations

--- src/Language/PureScript/Sugar/TypeClasses.hs
+++ src/Language/PureScript/Sugar/TypeClasses.hs
@@ -300,7 +300,8 @@ typeClassMemberToDictionaryAccessor mn name args (TypeDeclaration (TypeDeclarati
       dictIdent = Ident "dict"
       dictObjIdent = Ident "v"
       ctor = ConstructorBinder ss (coerceProperName . dictTypeName <$> className) [VarBinder ss dictObjIdent]
-      acsr = Accessor (mkString $ runIdent ident) (Var ss (Qualified ByNullSourcePos dictObjIdent))
+      -- N.B. changing this from ByNullSourcePos to the real source pos to hopefully make conversion to typed CoreFn AST work
+      acsr = Accessor (mkString $ runIdent ident) (Var ss (Qualified {- -ByNullSourcePos -} (BySourcePos $ spanStart ss) dictObjIdent))
       visibility = second (const TypeVarVisible) <$> args
   in ValueDecl sa ident Private []
     [MkUnguarded (
