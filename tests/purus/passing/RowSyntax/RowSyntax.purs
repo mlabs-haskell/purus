1de91e0a04190ee7295ec2c17922275f1a6e884f
gnumonik <gnumonik@protonmail.com>
2024-05-25 00:12:34 -0400

    more row syntax tests

--- tests/purus/passing/RowSyntax/RowSyntax.purs
+++ tests/purus/passing/RowSyntax/RowSyntax.purs
@@ -1,5 +1,12 @@
 module RowSyntax where
 
+type OneRow :: Row Type
+type OneRow = [one :: Int]
+
+type SomeRow :: Row Type
+type SomeRow = [inn'it :: Int, stirring :: String]
+
+
 class IsARow (r :: Row Type)
 instance IsARow [hello :: String]
 
@@ -13,3 +20,7 @@ aRowProxy = RowProxy
 
 moreFields :: RowProxy [field1 :: Int, field2 :: String, field3 :: Boolean]
 moreFields = RowProxy
+
+type TestRecord1 = {foob :: String, ar :: Int}
+
+type TestRecord2 = Record [foob :: String, ar :: Int]
