d9d988971b9e165585a267d12e4e0aff323fd8df
gnumonik <sean@mlabs.city>
2024-09-08 21:13:28 -0400

    Fixed bug in AST->CoreFn machinery (I hope)

--- tests/purus/passing/Validator/Validator.purs
+++ tests/purus/passing/Validator/Validator.purs
@@ -7,6 +7,7 @@ equalsPKH (PubKeyHash bs1) (PubKeyHash bs2) = Builtin.equalsByteString bs1 bs2
 validateIfSignedBy :: forall (a :: Type) (b :: Type). PubKeyHash -> a -> b -> Builtin.BuiltinData -> Boolean
 validateIfSignedBy pkh _ _ cxtData = anyList (equalsPKH pkh) signatories 
   where
-    ScriptContext cxt = deserializeScriptContext cxtData
+    scxt = deserializeScriptContext cxtData
+    ScriptContext cxt = scxt
     TxInfo info = cxt.txInfo
     signatories = info.signatories
