17991f57fbc90c2bbbfb9e00a7fa80284ab9a6e7
Koz Ross <koz.ross@retro-freedom.nz>
2024-09-26 09:13:49 +1200

    Attempt 1.31

--- default.nix
+++ default.nix
@@ -4,8 +4,8 @@
       cardanoPackages = pkgs.fetchFromGitHub {
         owner = "input-output-hk";
         repo = "cardano-haskell-packages";
-        rev = "3df392af2a61d61bdac1afd9c3674f27d6aa8efc"; # branch: repo
-        hash = "sha256-vvm56KzA6jEkG3mvwh1LEdK4H4FKxeoOJNz90H8l8dQ=";
+        rev = "149e5f9c0519927a79c0e3132af3ff22b795ac0c"; # branch: repo
+        hash = "";
       };
 
       purus = config.libHaskell.mkPackage {
